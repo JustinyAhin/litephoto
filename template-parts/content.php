fb0fc449b0341ec592bc380536b6adb8c2cce1b2
justin <justiny.ahinon@gmail.com>
2019-09-09 18:15:40 +0100

    Update the posts display on homepage

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,12 +10,19 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
-	<div class="post-thumbnail-container" style="background-image:url('<?php the_post_thumbnail_url(); ?>')";>
-		<div class="post-title-overlay">
-			<?php esc_html_e( the_title( '<h2 class="post-title">
-				<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ), 'litephoto' ); ?>
+	<?php if( has_post_thumbnail() ) : ?>
+
+		<div class="post-thumbnail-container" style="background-image:url('<?php the_post_thumbnail_url(); ?>')";>
+
+	<?php else: ?>
+
+		<div class="post-thumbnail-container" style="background:#fefefe;">
+			
+	<?php endif; ?>
+			<div class="post-title-overlay">
+				<?php esc_html_e( the_title( '<h2 class="post-title">
+					<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ), 'litephoto' ); ?>
+			</div>
 		</div>
-	</div>
 
 </article><!-- #post-<?php the_ID(); ?> -->
